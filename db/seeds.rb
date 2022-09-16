8c8696745d0ae023c229a22d19fdda68ef02ed4a
mateo soto <mateo.sotoa@udea.edu.co>
2022-09-15 20:07:37 -0500

    Ejercicio FULL

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Creando Registros"
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Buenos Aires Penthouse',
+  address: 'Calle 13',
+  description: 'Amplio espacio',
+  price_per_night: 100,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Manrique',
+  address: '10 c sur',
+  description: 'Lindo lugar',
+  price_per_night: 67,
+  number_of_guests: 7
+)
+Flat.create!(
+  name: 'Evg',
+  address: 'av 23',
+  description: 'Aire Libre',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+puts "Registros Creados"
